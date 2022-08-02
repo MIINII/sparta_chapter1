a899d44ed85246e3de9a57b84b4b70bdefc33887
Hwang-Junsu <hjs77728@gmail.com>
2022-08-02 16:02:59 +0900

    test

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from pymongo import MongoClient
+
+client = MongoClient('mongodb+srv://chapter01:chapter01@chapter01.lnhgc.mongodb.net/?retryWrites=true&w=majority')
+db = client.chapter01
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    cafe_list = list(db.cafes.find({}))
+    return render_template("cafes.html", cafe_list=cafe_list)
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
