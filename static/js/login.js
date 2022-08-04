5784e1bf2e70acc09b77b2b82aec6d54cd169174
[MIINII] <[ft.dawn.o@gmail.com]>
2022-08-04 09:20:03 +0900

    html수정

--- static/js/login.js
+++ static/js/login.js
@@ -1,5 +1,5 @@
 function sign_up() {
-    window.location.href = '/register'
+  window.location.href = '/register'
 }
 
 function sign_in() {
@@ -9,23 +9,24 @@ function sign_in() {
   if (userid == '' || password == '') {
     $('#help-id').text('아이디를 입력해주세요.').focus().removeClass('is-safe').addClass('is-danger')
     $('#help-password').text('비밀번호를 입력해주세요.').focus().removeClass('is-safe').addClass('is-danger')
-    $('#input-username')
+    $('#input-userid')
     return
   } else {
-    $('#help-id, #help-password').text('')
+    // $('#help-id, #help-password').text('')
+    $('#help-id').addClass('is-loading')
   }
 
   $.ajax({
     type: 'POST',
-    url: '/sign_in',
+    url: '/api/login',
     data: {
       userid_give: userid,
       password_give: password,
     },
     success: function (response) {
       if (response['result'] == 'success') {
-        $.cookie('mytoken', response['token'], {path: '/'});
-        window.location.href = '/main'
+        $.cookie('mytoken', response['token'], { path: '/' })
+        window.location.replace('/')
       } else {
         alert(response['msg'])
       }
